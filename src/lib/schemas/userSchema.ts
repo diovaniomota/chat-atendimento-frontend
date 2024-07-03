488c23a954c501b243e53febd83b7b4b87a19fa7
Diovanio Mota <diovaniomota8@gmail.com>
2024-07-03 13:51:00 -0300

    main

--- src/lib/schemas/userSchema.ts
+++ src/lib/schemas/userSchema.ts
@@ -5,7 +5,7 @@ export const updateUserSchema = z.object({
     name: z.string().min(1, { message: "Nome obrigatório" }).max(80, { message: "Nome muito grande" }),
     email: z.string().email({ message: "Email inválido" }).max(254, { message: "Email muito grande" }),
     password: z.string().max(80, { message: "Senha muito grande" }).refine(value => !value || /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9\s]).+$/.test(value), {
-        message: "A senha deve conter pelo menos uma letra, um número e um caractere especial"
+        message: "A senha deve conter pelo menos uma letra, um número e um caractere!"
     }),
     confirm_password: z.string()
 }).refine(data => data.password === data.confirm_password, {
