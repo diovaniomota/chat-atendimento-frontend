6f0a311153e59543921977133be32d2d610027b1
Diovanio Mota <diovaniomota8@gmail.com>
2024-07-04 17:32:20 -0300

    main

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,58 @@
+"use server"
+
+import { SignInData, SignUpData } from "@/lib/schemas/authSchema"
+import { signIn, signUp } from "@/lib/requests"
+import { cookies } from "next/headers"
+import { json } from "node:stream/consumers"
+import { User } from '../../Types/User';
+import { redirect } from "next/navigation"
+
+export const handleSignIn = async (data: SignInData) => {
+    const response = await signIn(data)
+
+    if (response.data) {
+        cookies().set({
+            name: process.env.NEXT_PUBLIC_AUTH_KEY as string,
+            value: response.data.access_token,
+            httpOnly: true,
+            maxAge: 86400 * 7, //7dias = days
+        })
+    }
+    return response
+}
+
+export const handleSignUp = async (data: SignUpData) => {
+    const response = await signUp(data)
+
+    if (response.data) {
+        cookies().set({
+            name: process.env.NEXT_PUBLIC_AUTH_KEY as string,
+            value: response.data.access_token,
+            httpOnly: true,
+            maxAge: 86400 * 7, //7dias === days
+        })
+    }
+    return response
+}
+
+export const haldleGetUser = async () => {
+    const  authCookie = cookies().get(process.env.NEXT_PUBLIC_AUTH_KEY as string)?.value
+
+    const response = await fetch(process.env.NEXT_PUBLIC_AUTH_KEY + '/api/v1/accounts/me', {
+        headers: {
+            Authorization: `Bearer ${authCookie}`
+        }
+    })
+
+    const jsonResponse = await response.json()
+    const userData = jsonResponse.User
+
+    if (userData) return userData as User
+
+    return null
+}
+
+export const haldleSignOut = () => {
+    cookies().delete(process.env.NEXT_PUBLIC_AUTH_KEY as string)
+    redirect('/auth/signin')
+}
